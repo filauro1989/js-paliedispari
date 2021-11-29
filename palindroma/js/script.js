9b521151096c7d158c0590a90e149d4bd54b1c33
filauro1989 <filauro1989@gmail.com>
2021-11-29 13:54:13 +0100

    first commit

---
+++ palindroma/js/script.js
@@ -0,0 +1,24 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// (prima senza funzione e poi con funzione)
+
+let userWord = prompt('Inserisci una parola');
+// let splitUserWord = userWord.split('');
+// splitUserWord = splitUserWord.reverse();
+// let palindrome = splitUserWord.join('');
+
+let palindrome = palindromo(userWord);
+
+if (userWord == palindrome) {
+    alert('bravo!')
+} else {
+    alert('sbagliato!')
+}
+
+function palindromo(parola) {
+    let palindrome = userWord.split('').reverse().join('');
+    return palindrome;
+}
+console.log(userWord);
+console.log(palindrome);
